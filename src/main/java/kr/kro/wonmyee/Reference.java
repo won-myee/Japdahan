abf1e14252e1ad3e0b4ead5c7faf37651b890b29
won-myee <lmg1152@naver.com>
2024-12-12 16:14:24 +0900

    Release 1.0.1

--- src/main/java/kr/kro/wonmyee/Reference.java
+++ src/main/java/kr/kro/wonmyee/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
     public static final String MODID = "japdahan";
     public static final String NAME = "Japdahan";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String SERVER_PROXY_CLASS = "kr.kro.wonmyee.proxy.CommonProxy";
     public static final String CLIENT_PROXY_CLASS = "kr.kro.wonmyee.proxy.ClientProxy";
 
